e6c375d2e963580671c94e5e325cc91440fdb744
Nathan Nguyen <btnguyen@umich.edu>
2021-02-01 22:21:34 -0500

    Added image upload and some style changes

--- src/App.js
+++ src/App.js
@@ -2,9 +2,41 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 import * as ml5 from "ml5";
 
+function Image(props) {
+    function handleChange(event) {
+        if (event.target.files.length) {
+            props.setImage({
+                preview: URL.createObjectURL(event.target.files[0]),
+                raw: event.target.files[0]
+            });
+        }
+    }
+
+    return(
+        <div className="Image">
+            <label htmlFor="upload-button">
+                {props.image.preview ? (
+                    <div>
+                        <img src={props.image.preview} id="image" class="rounded" width="400"/><br />
+                        <h3>Click on the image to upload a new one.</h3>
+                    </div>
+                )
+                : (
+                    <h3 type="button" class="btn btn-outline-secondary">
+                        Upload an image to see magic happens!
+                    </h3>
+                )}
+            </label>
+            <input type="file" id="upload-button" style={{ display: 'none' }}
+            onChange={handleChange} />
+            <br />
+        </div>
+    )
+}
+
 function App() {
     const [predictions, setPredictions] = useState([]);
-    const image_url = "https://images.unsplash.com/photo-1508817628294-5a453fa0b8fb?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1350&q=80";
+    const [image, setImage] = useState({ preview: "", raw: "" });
     const classifier = ml5.imageClassifier('MobileNet', modelLoaded);
 
     function modelLoaded() {
@@ -12,9 +44,9 @@ function App() {
     }
 
     function classifyImg() {
-        const image = document.getElementById('image');
+        const temp_image = document.getElementById('image');
 
-        classifier.predict(image, 5, function(err, results) {
+        classifier.predict(temp_image, 5, function(err, results) {
             console.log(results);
             return results;
         })
@@ -25,14 +57,17 @@ function App() {
 
     useEffect(() => {
         classifyImg();
-    }, []);
+    }, [image]);
 
     return (
         <div className="App">
-            <h1>Image classifcation with ML5.js</h1>
-            <img src={ image_url } crossOrigin="anonymous" id="image" width="400" alt="" />
+            <h1>Image Classifcation with ml5.js</h1>
+            <Image image={image} setImage={setImage}/>
+            {predictions.length ? <h2>Top Predictions</h2>:<h2></h2>}
             <ol>
-                {predictions.map(prediction => <li key={prediction.label}>{prediction.label}: {prediction.confidence}</li>)}
+                {predictions.map(prediction => <li key={prediction.label}>
+                    <span style={{textTransform: 'capitalize'}}><b>{prediction.label.split(",")[0]}</b></span>
+                    {' '}with {((prediction.confidence * 10000) / 100).toPrecision(2)}% confidence.</li>)}
             </ol>
         </div>
     );
